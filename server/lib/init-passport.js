20f1f375ca7c4f2b666451f1b3e50e8ec61e8245
toeknee <toeknee@toeknee.io>
2016-10-09 00:10:39 +0000

    some libz

---
+++ server/lib/init-passport.js
@@ -0,0 +1,22 @@
+module.exports = function(app, passportConfigurator) {
+
+  var config = require('/home/ubuntu/top_ramen/providers.json');
+
+  passportConfigurator.init();
+
+  // see passport errors
+  app.use(require('express-flash')());
+
+  passportConfigurator.setupModels({
+    userModel: app.models.user,
+    userIdentityModel: app.models.userIdentity,
+    userCredentialModel: app.models.userCredential
+  });
+
+  for (var s in config) {
+    var c = config[s];
+    c.session = c.session !== false;
+    passportConfigurator.configureProvider(s, c);
+  }
+
+};
---
+++ server/lib/init-ramen.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const path = require('path');
+const lodash = require("lodash");
+const winston = require('winston');
+const Promise = require("bluebird");
+const Memcached = require('memcached');
+
+const LOG_DIR = process.env.npm_package_config_logDir || path.join("..", "..");
+
+Memcached.config.poolSize = 25;
+Memcached.config.timeout = 1000;
+Memcached.config.maxExpiration = 2592000;
+
+const memcached = new Memcached('localhost');
+
+const logger = new winston.Logger({ transports: [ new winston.transports.Console(), new winston.transports.File({ filename:  `${LOG_DIR}/winston.log`}) ] });
+
+module.exports = function(app) {
+
+  app._ramen = {};
+  app._ramen.lodash = lodash;
+  app._ramen.promise = Promise;
+  app._ramen.memcached = memcached;
+  app._ramen.logger = logger;
+
+  return app;
+
+};
